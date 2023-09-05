fae213698a9b5b98f22a138ef9b9943f672f4378
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-05 15:42:18 -0300

    create login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,10 +2,41 @@ import express from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { random, authentication } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try{
+    const {email, password} = req.body;
+    if (!email || !password) {
+      return res.sendStatus(400)
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+    if (!user){
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+    await user.save();
+
+    res.cookie('FERNANDA-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+
+    return res.status(200).json(user).end();
+
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(400);
+  }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
   try{
-    const { email, password, username} = req.body;
-    
+    const { email, password, username} = req.body;    
     if(!email || !password || !username) {
       return res.sendStatus(400);
     }
@@ -24,7 +55,6 @@ export const register = async (req: express.Request, res: express.Response) => {
         password: authentication(salt, password),
       },
     });
-
     return res.status(200).json(user).end();
   } catch (error){
     console.log(error);
