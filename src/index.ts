8dfa6c63b51d770460ebf0a16d30f2acc8ddb4de
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-04 23:08:01 -0300

    setup mongoDB

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express();
 
 app.use(cors({
@@ -18,5 +21,11 @@ app.use(cookieParser());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-  console.log('Server running on http://localhost:8080/');
+  console.log("server running on port 8080");
 })
+
+const MONGO_URL=process.env.MONGO_URL
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
