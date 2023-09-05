f17d68cbeb0e9308327b95ca45f03ab2e6df7dfc
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-05 16:43:09 -0300

    add delete user route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getUsers} from "../db/users";
+import {deleteUserById, getUserById, getUsers} from "../db/users";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
   try{
@@ -8,6 +8,19 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
     
   } catch (error){
     console.log(error);
-    return res.sendStatus(400)
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await deleteUserById(id);
+    return res.json(deletedUser);
+
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
   }
 }
+
